ae0cb425a8c79fda72cfb7a2a395f9a361ccdf71
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2024-10-15 22:04:08 +0530

    adding javascript file

---
+++ script.js
@@ -0,0 +1,45 @@
+const output = document.getElementById("output");
+const btn = document.getElementById("btn");
+const input = document.getElementById("userInput");
+
+let x = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    
+    const userGuess = Number(input.value);
+    attempts++;
+
+    if (!input.value) {
+        output.innerHTML = "Enter A Number!!";
+        output.style.color = "red";
+        return;
+    } 
+    if (userGuess < 1 || userGuess > 100) {
+        output.innerHTML = "Number must be between 1 and 100!";
+        output.style.color = "red";
+        return;
+    }
+
+    if (userGuess === x) {
+        output.innerHTML = `You guessed Right! Your number is ${x}`;
+        output.style.color = "#00e300";
+        document.body.style.pointerEvents = "none";
+
+        clearInterval(intervalId); 
+        let intervalId = setInterval(() => {
+            output.innerHTML += ".";
+        }, 500);
+        
+        setTimeout(() => {
+            location.reload(); 
+        }, 2000);
+    } else if (userGuess < x) {
+        output.innerHTML = "You guessed Low!";
+        output.style.color = "orange";
+    } else {
+        output.innerHTML = "You guessed High!";
+        output.style.color = "orange";
+    }
+});
